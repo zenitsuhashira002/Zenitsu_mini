62014b2e036e3a182462425c1fb357151f8f0b7b
zenitsu.hashira <zenitsu.hashira.002@gmail.com>
2026-06-16 12:27:23 -0700

    amelioration main8

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ const socketIO = require('socket.io');
 //  CONFIG
 // ──────────────────────────────────────────────
 const CONFIG = {
-  ownerNumber : process.env.OWNER_NUMBER || '50935948231',
-  OWNER_JID   : (process.env.OWNER_NUMBER || '50935948231') + '@s.whatsapp.net',
+  ownerNumber : process.env.OWNER_NUMBER || '584168698003',
+  OWNER_JID   : (process.env.OWNER_NUMBER || '584168698003') + '@s.whatsapp.net',
   PREFIX      : process.env.PREFIX || '.',
   prefix      : process.env.PREFIX || '.',
   sessionDir  : './session',
